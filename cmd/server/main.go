10d71e3cf25a3e1aba109ebcf37d6ab5ff741c96
AnxVit <113212884+AnxVit@users.noreply.github.com>
2026-03-21 19:07:59 +0300

    Revert "iter1/http-server"

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,3 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/AnxVit/metrics-server/internal/handler"
-	"github.com/AnxVit/metrics-server/internal/repository"
-	"github.com/AnxVit/metrics-server/internal/service"
-)
-
-func main() {
-	repo := repository.NewMemStorage()
-
-	service := service.NewService(repo)
-
-	handler := handler.NewHandler(service)
-
-	err := http.ListenAndServe(`:8080`, handler)
-	if err != nil {
-		panic(err)
-	}
-}
+func main() {}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/AnxVit/metrics-server
-
-go 1.24.0
