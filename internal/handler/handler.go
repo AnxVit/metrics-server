42981b85c34bf05a9fd43a5854e977abaf45bce7
Vitaliy <vcyvunin@gmail.com>
2026-05-03 10:22:01 +0300

    fix: update handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -14,6 +14,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/AnxVit/metrics-server/internal/handler/middleware"
+	"github.com/AnxVit/metrics-server/internal/logger"
 	models "github.com/AnxVit/metrics-server/internal/model"
 	"github.com/AnxVit/metrics-server/internal/service"
 )
@@ -44,6 +45,7 @@ func NewHandler(service iService, conn *pgxpool.Pool) *Handler {
 		r.Get("/", h.handleGetAll)
 		r.Post("/value", h.handleGetMetric)
 		r.Post("/update", h.handlePostMetric)
+		r.Post("/updates", h.handlePostMetrics)
 		r.Get("/value/{type}/{name}", h.handleGetMetricParameters)
 		r.Post("/update/{type}/{name}/{value}", h.handlePostMetricParameters)
 		r.Get("/ping", h.handlePing)
@@ -193,21 +195,27 @@ func (h *Handler) handleGetMetricParameters(w http.ResponseWriter, r *http.Reque
 }
 
 func (h *Handler) handlePostMetric(w http.ResponseWriter, r *http.Request) {
-	var req UpdateRequest
+	var req *models.Metrics
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
+		logger.Log.Warn(err.Error())
 		http.Error(w, "bad request format", 400)
 		return
 	}
 
-	if err := req.validate(); err != nil {
-		http.Error(w, err.Error(), 400)
+	if req.MType == "" || req.ID == "" {
+		http.Error(w, "bad request values", 400)
+		return
+	}
+
+	if req.Delta == nil && req.Value == nil {
+		http.Error(w, "value or delta should set", 400)
 		return
 	}
 
 	ctx := r.Context()
 
-	if err := h.service.SaveMetrics(ctx, req.Metrics); err != nil {
+	if err := h.service.SaveMetrics(ctx, []*models.Metrics{req}); err != nil {
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
@@ -252,6 +260,39 @@ func (h *Handler) handlePostMetricParameters(w http.ResponseWriter, r *http.Requ
 			http.Error(w, "bad metric value", http.StatusBadRequest)
 			return
 		}
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+}
+
+type UpdatesRequest []*models.Metrics
+
+func (h *Handler) handlePostMetrics(w http.ResponseWriter, r *http.Request) {
+	var req UpdatesRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "bad request format", 400)
+		return
+	}
+
+	for _, metric := range req {
+		if metric.MType == "" || metric.ID == "" {
+			http.Error(w, "bad request values", 400)
+			return
+		}
+
+		if metric.Delta == nil && metric.Value == nil {
+			http.Error(w, "value or delta should set", 400)
+			return
+		}
+	}
+
+	ctx := r.Context()
+
+	if err := h.service.SaveMetrics(ctx, req); err != nil {
 		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
