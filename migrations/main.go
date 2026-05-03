ce276c286eab36972320d9750d2bd2ca4c4446ce
Vitaliy <vcyvunin@gmail.com>
2026-05-03 07:48:03 +0300

    fix migrations

--- migrations/main.go
+++ migrations/main.go
@@ -3,11 +3,14 @@ package migrations
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/AnxVit/metrics-server/internal/logger"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
+	"go.uber.org/zap"
 )
 
 var (
@@ -44,17 +47,18 @@ func main() {
 func Migrate(dbstring, command, migrationDir string, arguments []string) {
 	db, err := goose.OpenDBWithDriver("postgres", dbstring)
 	if err != nil {
-		log.Fatalf("goose: failed to open DB: %v", err)
+		logger.Log.Warn("goose: failed to open DB", zap.Error(err))
+		return
 	}
 
 	defer func() {
 		if err := db.Close(); err != nil {
-			log.Fatalf("goose: failed to close DB: %v", err)
+			logger.Log.Warn("goose: failed to close DB:", zap.Error(err))
 		}
 	}()
 
 	ctx := context.Background()
 	if err := goose.RunContext(ctx, command, db, migrationDir, arguments...); err != nil {
-		log.Fatalf("goose %v: %v", command, err)
+		logger.Log.Warn(fmt.Sprintf("goose %v", command), zap.Error(err))
 	}
 }
